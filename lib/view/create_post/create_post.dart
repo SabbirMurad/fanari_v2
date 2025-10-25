9aeae0f8726f14709927a80f3fe9cc767f10625d
Sabbir Hassan <sbbir0087@gmail.com>
2025-10-25 21:15:42 +0600

    Added image upload process

--- lib/view/create_post/create_post.dart
+++ lib/view/create_post/create_post.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
 import 'dart:math';
 import 'dart:typed_data';
+import 'package:blurhash_ffi/blurhash_ffi.dart';
 import 'package:fanari_v2/constants/colors.dart';
+import 'package:fanari_v2/constants/credential.dart';
 import 'package:fanari_v2/routes.dart';
 import 'package:fanari_v2/widgets/bouncing_three_dot.dart';
 import 'package:fanari_v2/widgets/custom_dropdown.dart';
@@ -15,6 +17,8 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:video_compress/video_compress.dart';
 import 'package:video_player/video_player.dart';
 import 'package:fanari_v2/utils.dart' as utils;
+import 'package:http/http.dart' as http;
+import 'package:image/image.dart' as img;
 
 class CreatePostScreen extends StatefulWidget {
   const CreatePostScreen({super.key});
@@ -74,6 +78,54 @@ class _CreatePostScreenState extends State<CreatePostScreen> {
     VideoCompress.deleteAllCache();
 
     super.dispose();
+  }
+
+  Future<void> uploadImages(List<File> images) async {
+    var uri = Uri.parse('${AppCredentials.domain}/image');
+    var request = http.MultipartRequest('POST', uri);
+
+    for (int i = 0; i < images.length; i++) {
+      var image = images[i];
+
+      final bytes = await image.readAsBytes();
+      final decodedImage = img.decodeImage(bytes);
+
+      if (decodedImage == null) {
+        debugPrint('');
+        debugPrint('Error decoding image');
+        debugPrint('');
+        return;
+      }
+
+      final blur_hash = await BlurhashFFI.encode(
+        MemoryImage(bytes),
+        componentX: 4,
+        componentY: 3,
+      );
+
+      // Attach image
+      request.files.add(
+        await http.MultipartFile.fromPath(
+          'images', // <-- same key for all images
+          image.path,
+          filename: image.uri.pathSegments.last,
+        ),
+      );
+
+      // Attach metadata for that image (by index)
+      request.fields['width_$i'] = '${decodedImage.width}';
+      request.fields['height_$i'] = '${decodedImage.height}';
+      request.fields['blur_hash_$i'] = blur_hash;
+      request.fields['used_at_$i'] = 'Post';
+    }
+
+    var response = await request.send();
+
+    if (response.statusCode == 200) {
+      print('Upload successful');
+    } else {
+      print('Upload failed: ${response.statusCode}');
+    }
   }
 
   Widget _selectedImagesWidget() {
@@ -575,15 +627,26 @@ class _CreatePostScreenState extends State<CreatePostScreen> {
               ),
             ),
             Spacer(),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 12.w, vertical: 6.h),
-              decoration: BoxDecoration(
-                color: AppColors.primary,
-                borderRadius: BorderRadius.circular(6),
-              ),
-              child: Text(
-                'Post',
-                style: TextStyle(fontSize: 14.sp, color: AppColors.text),
+            GestureDetector(
+              onTap: () {
+                print('');
+                print('Uploading');
+                print('');
+                uploadImages(_selectedImages);
+                print('');
+                print('Done');
+                print('');
+              },
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 12.w, vertical: 6.h),
+                decoration: BoxDecoration(
+                  color: AppColors.primary,
+                  borderRadius: BorderRadius.circular(6),
+                ),
+                child: Text(
+                  'Post',
+                  style: TextStyle(fontSize: 14.sp, color: AppColors.text),
+                ),
               ),
             ),
           ],
